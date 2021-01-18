9c1e1e4dbffb361296f0df3dbe2001179a7c1d2d
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-01-17 20:03:04 -0500

    question and loops done need style and score

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,23 @@
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const choiceD = document.getElementById("D");
+
 const counter = document.getElementById("counter");
 const timer = document.getElementById("timer");
-
-const optA = document.getElementById("A");
-const optB = document.getElementById("B");
-const OptC = document.getElementById("C");
-const OptD = document.getElementById("C");
-
 const tracker = document.getElementById("tracker");
-const score = document.getElementById("Score");
-const questionTime = 10;
-const gaugeWidth = 150;
+const scoreD = document.getElementById("Score");
+const highscore = document.getElementById("highscore");
 let count = 0;
+  const questionTime = 10;
+const gaugeWidth = 150;
+const gaugeProgress = gaugeWidth/questionTime;
+let score = 0;
+
 let questions = [
   {
       question : "JavaScript is a ___ -side programming language.",
@@ -25,13 +29,13 @@ let questions = [
       correct : "C"
   },
   {
-    question : "Which of the following will write the message “Hello DataFlair!” in an alert box?",
+    question : "Commonly used data types do NOT include",
     
-    choiceA : "alertBox(“Hello DataFlair!”);",
-    choiceB : "alert(Hello DataFlair!);",
-    choiceC : "msgAlert(“Hello DataFlair!”);",
-    choiceD : "alert(“Hello DataFlair!”);",
-    correct : "D"
+    choiceA : "strings",
+    choiceB : " booleans",
+    choiceC : " alerts",
+    choiceD : "numbers",
+    correct : "B"
 },
 {
   question : "How do you find the minimum of x and y using JavaScript?",
@@ -43,7 +47,7 @@ let questions = [
   correct : "D"
 },
 {
-  question : "Which are the correct if statements to execute certain code if “x” is equal to 2?",
+  question : "Which are the correct if statements to execute certain code if x is equal to 2?",
   
   choiceA : "if(x 2)",
   choiceB : "if(x = 2)",
@@ -52,66 +56,19 @@ let questions = [
   correct : "C"
 },
 ];
+
 const lastQuestionIndex = questions.length -1;
 let runningQuestionIndex =0;
-//let lastQuestionIndex = questions.length -1;
-//let runningQuestionIndex = 0;
 
 function displayQuestion() {
-  let q = questions[runningQuestionIndex];
-  question.innerHTML = "<p>" +q.question+"<p>";
-  choiceA.innerHTML = q.choiceA;
-  choiceB.innerHTML = q.choiceB;
-  choiceC.innerHTML = q.choiceC;
-  choiceD.innerHTML = q.choiceD;
-}
-function progressRender(){
-  for (let qIndex =0; qIndex <=lastQuestionIndex; qIndex++){
-      progressRender.innerHTML += "<div class='prog' id=" +qIndex +"></div>";
-  }
-}
-function answerIsCorrect(){
-  document.getElementById(runningQuestionIndex).style.backgroundColor = "green"
-}
-function answerIsWrong(){
-  document.getElementById(runningQuestionIndex).style.backgroundColor = "red"
-}
-
-
-const gaugeProgressUnit = gaugeWidth/questionTime;
-  function counterRender(){
-    if( count <=questionTime){
-      counter.innerHTML = count;
-      timeGauge.style.width = gaugeProgressUnit * count + "px";
-      count++;
-    } else{
-      count = 0;
-      answerIsWrong();
-      if ( runningQuestionIndex < lastQuestionIndex){
-        runningQuestionIndex++;
-        questionRender();
-      }else{ clearInterval(Timer);
-              scoreRender();
-      }
-    }
+    let q = questions[runningQuestionIndex];
+    question.innerHTML = "<p>" + q.question +"<p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
   }
 
-  function checkAnswer(answer){
-    if (questions[runningQuestionIndex].correct == answer){
-        score++;
-        answerIsCorrect();
-    } else{
-        answerIsWrong();
-    }
-    if(runningQuestionIndex < lastQuestionIndex){
-      count = 0;
-      runningQuestionIndex++;
-      questionRender();
-    } else{
-        clearInterval(Timer);
-        scoreRender();
-    }
-  }
   start.addEventListener("click", startQuiz);
   function startQuiz(){
     start.style.display = "none";
@@ -121,8 +78,61 @@ const gaugeProgressUnit = gaugeWidth/questionTime;
     displayQuestion();
     quiz.style.display = "block";
   }
-  //fix
-function scoreRender(){
-  ServiceWorkerContainer.style.display="block";
-  let scorePerCent = Math.round(100* score / questions.length);
-}
+
+  function progressRender(){
+    for (let qIndex =0; qIndex <=lastQuestionIndex; qIndex++){
+        progressRender.innerHTML += "<div class='prog' id=" +qIndex +"></div>";
+    }
+  }
+
+  
+ 
+function counterRender(){
+    if( count <=questionTime){
+      counter.innerHTML = count;
+      timeGauge.style.width = gaugeProgress * count + "px" ;
+      count++;
+    } else{
+      count = 0;
+      answerIsWrong();
+      if(runningQuestionIndex < lastQuestionIndex){
+        runningQuestionIndex++;
+        displayQuestion();
+      } else{
+        clearInterval(Timer);
+        scoreRender();
+      }
+    }
+  }
+    function checkAnswer(answer){
+      if (questions[runningQuestionIndex].correct == answer){
+          score++;
+         
+      } else{
+    
+      }
+      count = 0;
+      if(runningQuestionIndex < lastQuestionIndex){
+        runningQuestionIndex++;
+        displayQuestion();
+      } else{
+          clearInterval(Timer);
+          scoreRender();
+      }
+    }
+   function scoreRender(){
+    
+    }
+    function highscores(){
+      var highscore = localStorage.getItem("highscore");
+
+      if(highscore !== null){
+          if (score > highscore) {
+              localStorage.setItem("highscore", score);      
+          }
+      }
+      else{
+          localStorage.setItem("highscore", score);
+      }
+    }
+    
